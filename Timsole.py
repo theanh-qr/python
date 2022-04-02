9b2eceb43a533fbce07bc2efc26da9938925c4ad
theanh-qr <theanhmlhl@gmail.com>
2022-04-02 13:36:02 +0700

    add commit

---
+++ Timsole.py
@@ -0,0 +1,24 @@
+import array as arr
+
+a = arr.array('d',[])
+int odd
+print("Mời bạn nhập 4 số nguyên !!!")
+print("Mời bạn nhập số thứ 1")
+s1=input()
+a.append(s1)
+print("Mời bạn nhập số thứ 2")
+s2=input()
+a.append(s2)
+print("Mời bạn nhập số thứ 3")
+s3=input()
+a.append(s3)
+print("Mời bạn nhập số thứ 4")
+s4=input()
+a.append(s4)
+
+for i in a:
+	if i%2 != 0
+		odd++
+print("Số số lẻ trong 4 số bạn vừa nhập là: ",odd)
+		
+	
